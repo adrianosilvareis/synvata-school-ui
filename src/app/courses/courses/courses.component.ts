ce7c7a0bb6426d9768e4e763fa79ad664f765e49
Adriano Reis <adriano_silvareis@hotmail.com>
2022-09-20 23:46:56 -0300

    feat: list courses

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { catchError, Observable, of } from 'rxjs';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
+
+import { Course } from '../model/course';
+import { CoursesService } from './../services/courses.service';
 
 @Component({
   selector: 'app-courses',
@@ -7,9 +13,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesComponent implements OnInit {
 
-  constructor() { }
+  courses$: Observable<Course[]>;
+
+  displayedColumns = ['name', 'students']
+
+  constructor(
+    private coursesServices: CoursesService,
+    public dialog: MatDialog
+    ) {
+    this.courses$ = this.coursesServices.list().pipe(catchError(error => {
+      this.onError('Error loading courses.');
+      return of([])
+    }))
+  }
 
   ngOnInit(): void {
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg,
+    });
   }
 
 }
