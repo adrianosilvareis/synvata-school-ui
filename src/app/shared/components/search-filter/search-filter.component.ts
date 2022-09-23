74f379193b428f4c08a65549983379366d403adf
Adriano Reis <adriano_silvareis@hotmail.com>
2022-09-22 23:05:32 -0300

    feat: added search filter component

---
+++ src/app/shared/components/search-filter/search-filter.component.ts
@@ -0,0 +1,57 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+
+export interface Display {
+  id: string;
+  name: string;
+}
+
+/**
+ * @title Display value autocomplete
+ */
+@Component({
+  selector: 'app-search-filter',
+  templateUrl: './search-filter.component.html',
+  styleUrls: ['./search-filter.component.scss']
+})
+export class SearchFilterComponent implements OnInit{
+  @Input() modelValue: string = '';
+  @Input() options: Display[] = [];
+  @Output() filter = new EventEmitter();
+
+  myControl = new FormControl<string | Display>('');
+  filteredOptions: Observable<Display[]>;
+
+  constructor() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => {
+        const name = typeof value === 'string' ? value : value?.name;
+        return name ? this._filter(name as string) : this.options.slice();
+      }),
+    );
+  }
+
+  displayFn(user: Display): string {
+    return user && user.name ? user.name : '';
+  }
+
+  private _filter(name: string): Display[] {
+    const filterValue = name.toLowerCase();
+    return this.options.filter(option => option.name.toLowerCase().includes(filterValue));
+  }
+
+  onSelect() {
+    this.filter.emit(this.myControl.value)
+  }
+
+  ngOnInit(): void {
+    if (this.modelValue) {
+      const option = this.options.find(option => option.id === this.modelValue) ?? ''
+      this.myControl.setValue(option)
+      this.myControl.disable()
+    }
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,20 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppMaterialModule } from './app-material/app-material.module';
 import { ErrorDialogComponent } from './components/error-dialog/error-dialog.component';
 import { ConfirmationDialogComponent } from './components/confirmation-dialog/confirmation-dialog.component';
+import { SearchFilterComponent } from './components/search-filter/search-filter.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [
     ErrorDialogComponent,
-    ConfirmationDialogComponent
+    ConfirmationDialogComponent,
+    SearchFilterComponent
   ],
   imports: [
     AppMaterialModule,
+    ReactiveFormsModule,
     CommonModule
   ],
   exports:[
-    ErrorDialogComponent
+    ErrorDialogComponent,
+    ConfirmationDialogComponent,
+    SearchFilterComponent
   ]
 })
 export class SharedModule { }
